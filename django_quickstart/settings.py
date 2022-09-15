9169a2ba937bfc768523dfc16f73aa0c706271de
Mateusz Lorenc <MALOR@orsted.com>
2022-09-15 13:30:25 +0200

    fixed key

--- django_quickstart/settings.py
+++ django_quickstart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0$kucup$vcyc-r&-+pmxd&)g5y3m$lc+!+f9s&rjre25=z4(t@'
+SECRET_KEY = 'django-mock-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
