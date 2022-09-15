dc159dc3f7f484d9ee098447fcb836cccd213185
Mateusz Lorenc <MALOR@orsted.com>
2022-09-15 17:06:33 +0200

    initialised pinger app

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
+from rest_framework import viewsets
+from rest_framework import permissions
+from challenges.serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.models import User, Group
 
 challenge_list = {
     '1': 'do this',
@@ -16,3 +20,20 @@ def index(request):
 def monthly_challenges(request, month):
     return HttpResponse(f'its {month}')
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
