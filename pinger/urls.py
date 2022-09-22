edd8886fb620b2606a1df253a8d0b22036ded2ba
Mateusz Lorenc <MALOR@orsted.com>
2022-09-22 16:57:47 +0200

    initialised jwt

--- pinger/urls.py
+++ pinger/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework import routers
 from . import views
 
@@ -9,5 +10,10 @@ router.register('pingprofile', views.PingerProfileViewSet)
 urlpatterns = [
     path("ip/", views.ping_ips),
     path("history/", views.view_ping_history),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    '',
+    # ...
+
+    path(r'^api-token-auth/', obtain_jwt_token),
+
 ]
